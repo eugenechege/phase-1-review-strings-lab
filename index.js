9ef2ec590c4c45ce925adcb0ecd31e32fe38afa5
Eugene <eugene.chege@student.moringaschool.com>
2025-03-08 02:19:11 +0300

    update review strings

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code in this file!
+const currentUser = "Grace hopper"
+
+const welcomeMessage = 'Welcome to Flatbook, + ${Grace hopper}!'
+const excitedWelcomeMessage = welcomeMessage . toUpperCase()
+const shortGreeting = "Welcome, ${G.slice(0,1)}!"
